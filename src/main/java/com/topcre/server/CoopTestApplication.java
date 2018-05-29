c90f621b162b3c1be925c0d6506ea34be60d9e3a
JasonChoi <ccc11203@gmail.com>
2018-05-29 11:04:07 +0900

    initial commit

---
+++ src/main/java/com/topcre/server/CoopTestApplication.java
@@ -0,0 +1,12 @@
+package com.topcre.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoopTestApplication.class, args);
+	}
+}
